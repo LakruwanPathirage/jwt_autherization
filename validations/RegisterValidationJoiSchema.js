f0af6a47c9b8540a4e8742e691fbcc77c9ba4d6b
LakruwanPathirage <lakruwanpathirage01@gmail.com>
2022-01-23 18:30:42 +0530

    joi schema for registration

---
+++ validations/RegisterValidationJoiSchema.js
@@ -0,0 +1,9 @@
+const Joi = require("@hapi/joi");
+
+const schema = {
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+};
+
+module.exports = schema;
