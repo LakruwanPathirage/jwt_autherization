29b8497ebea4330565e1224e7056e2ad2f51420b
LakruwanPathirage <lakruwanpathirage01@gmail.com>
2022-01-23 18:29:58 +0530

    change get route to post route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const authController = require("../controllers/authController");
 
-router.get("/register", authController.autheticationLogic);
+router.post("/register", authController.autheticationLogic);
 
 module.exports = router;
