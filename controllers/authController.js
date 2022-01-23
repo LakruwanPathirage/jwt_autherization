87df104642d434bcd7ea61f39f0865318a38ab11
LakruwanPathirage <lakruwanpathirage01@gmail.com>
2022-01-23 18:29:39 +0530

    user registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,24 @@
-const autheticationLogic = (req, res) => {
-  console.log("hello");
+const User = require("../schemas/user");
+
+const autheticationLogic = async (req, res, next) => {
+  console.log(req.body);
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const saveduser = await user.save();
+    res.status(200).json({
+      data: saveduser,
+      message: "sucess",
+    });
+  } catch (err) {
+    res.status(400).json({
+      data: null,
+      message: err.message,
+    });
+  }
 };
 
-const miidle = function (req, res, next) {
-  console.log("middleware");
-  next();
-};
-
-module.exports = { autheticationLogic, miidle };
+module.exports = { autheticationLogic };
