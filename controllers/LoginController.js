d6b31019cd1ac32a704e56e690d8c9768a1c3e2c
LakruwanPathirage <lakruwanpathirage01@gmail.com>
2022-01-24 17:49:30 +0530

    add login controller logic

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -2,7 +2,23 @@ const User = require("../schemas/user");
 const bcrypt = require("bcryptjs");
 
 const autheticationLogin = async (req, res, next) => {
-  console.log("Login controller");
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return res
+      .status(400)
+      .json({ data: null, message: "email is not in the system" });
+  }
+
+  const validpassword = bcrypt.compare(req.body.password, user.password);
+
+  if (!validpassword) {
+    return res
+      .status(400)
+      .json({ data: null, message: "paasword is not valid" });
+  }
+
+  return res.json({ message: "Login succesful" });
 };
 
 module.exports = { autheticationLogin };
