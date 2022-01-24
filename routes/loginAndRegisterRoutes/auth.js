e9e92e919bb9e2273de862db54492e68e4f46576
LakruwanPathirage <lakruwanpathirage01@gmail.com>
2022-01-24 19:53:04 +0530

    movw auth file

---
+++ routes/loginAndRegisterRoutes/auth.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const RegisterController = require("../../controllers/RegisterController");
+const LoginController = require("../../controllers/LoginController");
+const {
+  RegisterValidation,
+  LoginValidation,
+} = require("../../validations/validation");
+
+router.post(
+  "/register",
+  RegisterValidation,
+  RegisterController.autheticationRegister
+);
+router.post("/login", LoginValidation, LoginController.autheticationLogin);
+
+module.exports = router;
